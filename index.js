f5f067f5c52845cb3948ebd4caec9cb06ad0b295
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-01 21:32:07 -0300

    Endpoint '/mensagens' criado

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
+
+app.get('/mensagens', function (req, res) {
+  res.send('Exibir todas as mensagens.')
+})
+
 app.listen(3000)
